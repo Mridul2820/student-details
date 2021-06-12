1ba3ac2ccc07db5c9675bf86b5665ee2238efdb6
Mridul2820 <immridulpanda2820@gmail.com>
2021-06-12 11:13:59 +0530

    login and sign up page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import GlobalStyle from './GlobalStyles'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import DashBoard from './pages/DashBoard'
 
 const App = () => {
     return (
         <div>
             <GlobalStyle />
-            My App
+            <Router>
+                <Switch>
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/" component={DashBoard} exact />
+                </Switch>
+            </Router>
         </div>
     )
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Signup = () => {
+    return (
+        <SignupContainer>
+            <SignupMain>
+                <h1>Create an Account</h1>
+                <Form>
+                    <InputGroup>
+                        <label>Email ID</label>
+                        <input type="email" />
+                    </InputGroup>
+                    <InputGroup>
+                        <label>Password</label>
+                        <input type="password" />
+                    </InputGroup>
+                    <Button type="submit">Submit</Button>
+                </Form>
+                <LoginLink>
+                    <p>Already have an acoount?</p>
+                    <Link to="/login">Log in</Link>
+                </LoginLink>
+                
+            </SignupMain>
+        </SignupContainer>
+    )
+}
+
+const SignupContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: #fff;
+`
+const SignupMain = styled.div`
+    background: #eee;
+    padding: 30px 40px;
+    border: 1px solid #444;
+
+    h1 {
+        font-size: 24px;
+        text-align: center;
+        margin-bottom: 15px;
+    }
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const InputGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+
+    label {
+        margin-bottom: 5px;
+        font-size: 14px;
+    }
+
+    input {
+        padding: 5px;
+        width: 250px;
+    }
+`
+
+const Button = styled.button`
+    padding: 5px 15px;
+    background: #ccc;
+    border: 1px solid #444;
+    margin-top: 10px;
+`
+
+const LoginLink = styled.div`
+    text-align: center;
+    margin-top: 15px;
+
+    a {
+        color: blue;
+    }
+`
+
+export default Signup

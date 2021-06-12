bf0af5d5acf00e94e8e7fcab2389f364653e1561
Mridul2820 <immridulpanda2820@gmail.com>
2021-06-13 00:26:32 +0530

    add student

--- src/components/StudentAdd.js
+++ src/components/StudentAdd.js
@@ -1,11 +1,117 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Button } from '../GlobalStyles'
 
 const StudentAdd = () => {
     return (
-        <div>
-            student add
-        </div>
+        <Container>
+            <h1>Add Student</h1>
+
+            <InputGrp>
+                <label>Full Name</label>
+                <input type="text" placeholder="Name" />
+            </InputGrp>
+            <InputGrp>
+                <label>Date of Birth</label>
+                <input type="date" />
+            </InputGrp>
+            <InputGrp>
+                <label>School</label>
+                <select name="School" id="school">
+                    <option value="" disabled defaultValue>School</option>
+                    <option value="Lead School">Lead School</option>
+                    <option value="School 2">School 2</option>
+                    <option value="School 3">School 3</option>
+                </select>
+            </InputGrp>
+            <InputGrp>
+                <label>Class</label>
+                <select name="Class" id="class">
+                    <option value="" disabled defaultValue>Class</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </InputGrp>
+            <InputGrp>
+                <label>Division</label>
+                <select name="Division" id="division">
+                    <option value="" disabled defaultValue>Division</option>
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="C">C</option>
+                </select>
+            </InputGrp>
+            <InputGrp>
+                <label>Status</label>
+                <Radio>
+                    <input type="radio" id="active" name="status" value="active"/>
+                    <label>Active</label>
+                    <input type="radio" id="inactive" name="status" value="inactive"/>
+                    <label>Inactive</label>
+                </Radio>
+            </InputGrp>
+            <InputGrp>
+                <label></label>
+                <Button>Save</Button>
+            </InputGrp>
+            
+        </Container>
     )
 }
+
+const Container = styled.div`
+    padding: 20px 50px;
+    width: 100%;
+
+    h1 {
+        border-bottom: 1px solid #000;
+        font-size: 22px;
+    }
+
+    ${Button} {
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-right: 5px;
+        }
+    }
+`
+
+const InputGrp  =styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+
+    label {
+        width: 150px;
+    }
+
+    input, select {
+        padding: 5px;
+        margin-right: 10px;
+        width: 150px;
+    }
+
+    option[value=""][disabled] {
+        display: none;
+    }
+`
+
+const Radio = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    input {
+        width: unset;
+    }
+
+    label {
+        width: unset;
+        margin-right: 30px;
+    }
+`
 
 export default StudentAdd
--- src/components/studentview/ViewHeader.js
+++ src/components/studentview/ViewHeader.js
@@ -9,19 +9,19 @@ const ViewHeader = () => {
                 <input type="text" placeholder="Name" />
                 <input type="number" placeholder="Age" />
                 <select name="School" id="school">
-                    <option value="" disabled selected>School</option>
+                    <option value="" disabled defaultValue>School</option>
                     <option value="Lead School">Lead School</option>
                     <option value="School 2">School 2</option>
                     <option value="School 3">School 3</option>
                 </select>
                 <select name="Class" id="class">
-                    <option value="" disabled selected>Class</option>
+                    <option value="" disabled defaultValue>Class</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
                 </select>
                 <select name="Division" id="division">
-                    <option value="" disabled selected>Division</option>
+                    <option value="" disabled defaultValue>Division</option>
                     <option value="A">A</option>
                     <option value="B">B</option>
                     <option value="C">C</option>
